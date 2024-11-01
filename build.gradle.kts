8aa8c784e5530dd12ded3f9af323566a30e2bda2
usrmrz <usrmrz@gmail.com>
2024-11-01 23:10:12 +0200

    remove autonomousapps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,4 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.autonomousapps) apply false
     }
