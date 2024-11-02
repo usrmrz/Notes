d1beb166355a71275818c51cdbcac37c0e8151b9
usrmrz <usrmrz@gmail.com>
2024-11-02 02:27:40 +0200

    add NoteTextFieldState continue with AddEditNoteViewModel

--- app/src/main/java/dev/usrmrz/notes/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
+++ app/src/main/java/dev/usrmrz/notes/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
@@ -2,15 +2,36 @@ package dev.usrmrz.notes.feature_note.presentation.add_edit_note
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.usrmrz.notes.feature_note.domain.model.Note
 import dev.usrmrz.notes.feature_note.domain.use_case.NoteUseCases
+import kotlinx.coroutines.flow.MutableSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEditNoteViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
-    private val _noteTitle = mutableStateOf((""))
-    val noteTitle: State<String> = _noteTitle
+
+    private val _noteTitle = mutableStateOf(NoteTextFieldState(
+        hint = "Enter title..."
+    ))
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
+
+    private val _noteContent = mutableStateOf(NoteTextFieldState(
+        hint = "Enter content..."
+    ))
+    val noteContent: State<NoteTextFieldState> = _noteContent
+
+    private val _noteColor = mutableStateOf(Note.noteColors.random().toArgb())
+    val noteColor: State<Int> = _noteColor
+
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+
+    sealed class UiEvent {
+        data class ShowSnackbar(val message: String): UiEvent()
+        object SaveNote: UiEvent()
+    }
 }
